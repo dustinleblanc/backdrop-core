backdrop/backdrop

Issue #2402 Add the current page as the last breadcrumb.

core/themes/basis/template.php
@@ -78,6 +78,7 @@ function basis_preprocess_header(&$variables) {
  * @see theme_breadcrumb().
  */
 function basis_breadcrumb($variables) {
+  $page_title = backdrop_get_title();
   $breadcrumb = $variables['breadcrumb'];
   $output = '';
   if (!empty($breadcrumb)) {
@@ -85,7 +86,7 @@ function basis_breadcrumb($variables) {
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output .= '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-    $output .= '<ol><li>' . implode('</li><li>', $breadcrumb) . '</li></ol>';
+    $output .= '<ol><li>' . implode('</li><li>', $breadcrumb) . '</li><li><p>' . $page_title . '</p></li></ol>';
     $output .= '</nav>';
   }
   return $output;

dustinleblanc/backdrop-core

themes/basis/template.php
@@ -78,6 +78,7 @@ function basis_preprocess_header(&$variables) {
  * @see theme_breadcrumb().
  */
 function basis_breadcrumb($variables) {
+  $page_title = backdrop_get_title();
   $breadcrumb = $variables['breadcrumb'];
   $output = '';
   if (!empty($breadcrumb)) {
@@ -85,7 +86,7 @@ function basis_breadcrumb($variables) {
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output .= '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-    $output .= '<ol><li>' . implode('</li><li>', $breadcrumb) . '</li></ol>';
+    $output .= '<ol><li>' . implode('</li><li>', $breadcrumb) . '</li><li><span>' . $page_title . '</span></li></ol>';
     $output .= '</nav>';
   }
   return $output;